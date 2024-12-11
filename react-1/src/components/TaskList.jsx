f9719c8ace1f25e4a9eea706611645de28d9be72
Gaspi04 <agustingastanaga1802@gmail.com>
2024-12-10 21:49:23 -0300

    kgkhf

--- react-1/src/components/TaskList.jsx
+++ react-1/src/components/TaskList.jsx
@@ -1,11 +1,20 @@
 import React, { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
-import { Button } from "./styles/DeleteAllButton.style";
+import { List, ListItem, DeleteButton } from "./styles/TaskList.style";
 
-const DeleteAllButton = () => {
-  const { clearTasks } = useContext(TaskContext);
+const TaskList = () => {
+  const { tasks, removeTask } = useContext(TaskContext);
 
-  return <Button onClick={clearTasks}>Borrar todo</Button>;
+  return (
+    <List>
+      {tasks.map((task, index) => (
+        <ListItem key={index}>
+          {task}
+          <DeleteButton onClick={() => removeTask(task)}>Borrar</DeleteButton>
+        </ListItem>
+      ))}
+    </List>
+  );
 };
 
-export default DeleteAllButton;
+export default TaskList;
